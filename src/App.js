793f393371334877b5c18dcd7ec7f3a3ebd71502
Erik Heikkila <erikjheikkila@gmail.com>
2021-08-29 15:02:54 -0700

    removed functionality

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import NavBar from "./components/NavBar.js";
 import ItemsList from "./components/ItemsList.js";
 
-export default function App () {
-  const [items, setItems] = useState({
-    apples: 0,
-    bananas: 0,
-    pears: 0,
-    oranges: 0
-  })
-
-  const addItem = item => {
-    const updatedItems = {...items};
-    updatedItems[item]++;
-    setItems(updatedItems);
-  };
-
-  const subtractItem = item => {
-    const updatedItems = {...items};
-    if (items[item]) {
-      updatedItems[item]--;
-    }
-    setItems(updatedItems);
-  };
-
-  const handleReset = () => {
-    const updatedItems = {...items};
-    for (const key in updatedItems) {
-      updatedItems[key] = 0;
-    }
-    setItems(updatedItems);
-  };
-
-  let stuff = Object.entries(items).filter(entry => {
-    return entry[1] > 0;
-  }).length
-  console.log(stuff)
-
-  return (
-    <div>
-      <NavBar
-        totalItems={Object.entries(items).filter(entry => entry[1] > 0).length}
-      />
-      <main className="container">
-        <ItemsList
-          items={items}
-          handleReset={handleReset}
-          addItem={addItem}
-          subtractItem={subtractItem}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar
+          totalItems={0}
         />
-      </main>
-    </div>
-  );
-
+        <main className="container">
+          <ItemsList />
+        </main>
+      </div>
+    );
+  }
 }
+
+export default App;
 
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,33 +1,14 @@
 import React from "react";
-import Item from "./Item.js";
 
-export default function ItemsList({
-  items = [],
-  handleReset,
-  addItem,
-  subtractItem
-}) {
+export default function ItemsList() {
 
   return (
     <div>
       <button
         className="btn btn-success m-2"
-        onClick={handleReset}
-        disabled={items.length === 0 ? "disabled" : ""}
       >
         <i className="fa fa-refresh" aria-hidden="true" />
       </button>
-      <ul className="list row">
-        {Object.keys(items).map((item, index) => (
-          <Item
-            key={index}
-            item={item}
-            value={items[item]}
-            addItem={addItem}
-            subtractItem={subtractItem}
-          />
-        ))}
-      </ul>
     </div>
   );
 }
